417806583017e628a2af3ccc6693c0b37948854d
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-11-22 15:24:20 +0200

    Change post toast button style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import styles from './Button.module.css';
 
-function Button({ className = '', ...delegated }) {
+function Button({ className = '', disabled, ...delegated }) {
   return (
     <button
-      className={`${styles.button} ${className}`}
+      className={`${styles.button} ${className} ${disabled && styles.button__disabled}`}
+      disabled={disabled}
       {...delegated}
     />
   );
